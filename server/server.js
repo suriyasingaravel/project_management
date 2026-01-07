a899df73905e15625140fe644f4a73c15ad4b12b
suriya <suriya@hogr.in>
2026-01-07 23:16:01 +0530

    fixed final changes in server for workspace creation

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ import cors from "cors";
 import { clerkMiddleware } from "@clerk/express";
 import { serve } from "inngest/express";
 import { inngest, functions } from "./inngest/index.js";
-import workspaceRouter from "./routes/workSpaceRoutes.js";
+import workspaceRouter from "./routes/workspaceRoutes.js";
 import { protect } from "./middlewares/authMiddleware.js";
 
 const app = express();
